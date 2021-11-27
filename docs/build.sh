d7afbd210cbb76f97ccae0d7d11a62d87e496a1a
LosAngeles971 <LosAngeles1791@gmail.com>
2021-11-27 17:46:39 +0100

    md docs

--- docs/build.sh
+++ docs/build.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-go get code.rocketnine.space/tslocum/godoc-static
-go get golang.org/x/tools/cmd/godoc
-mkdir -p ./html
-godoc-static \
-    -site-name="DataQ" \
-    -site-description-file=../DESCRIPTION.md \
-    -destination=./html \
-    ..\
+go install github.com/princjef/gomarkdoc/cmd/gomarkdoc@latest
+gomarkdoc --output DataQ.md ..\
